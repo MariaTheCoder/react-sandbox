9b5c93a7322c7487cea16cf3b35e707ce395cb5b
Maria T. Andersen <mteilmann93@gmail.com>
2022-09-25 21:33:17 +0200

    Add button which is to be used when a user desires to edit a message

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,10 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEraser, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function List(props) {
   return (
     <li>
       {props.mes}
+      <button onClick={() => console.log("edit me")}>
+        EDIT <FontAwesomeIcon icon={faEraser} />
+      </button>
       <button onClick={() => props.deleteItem(props.index)}>
         DELETE <FontAwesomeIcon icon={faTrash} />
       </button>
