62ab1e8f546489319934e81e264a4937cdb4f9cc
Maria T. Andersen <mteilmann93@gmail.com>
2022-09-25 20:57:13 +0200

    If no the messages state variable is empty, display a message on the screen saying so.

--- src/Components/List.js
+++ src/Components/List.js
@@ -2,15 +2,21 @@ import Message from "./Message";
 
 export default function List(props) {
   return (
-    <ul className="List">
-      {props.messages.map((mes, index) => (
-        <Message
-          key={index}
-          index={index}
-          mes={mes}
-          deleteItem={props.deleteItem}
-        />
-      ))}
-    </ul>
+    <>
+      {props.messages.length <= 0 ? (
+        <p>No messages</p>
+      ) : (
+        <ul className="List">
+          {props.messages.map((mes, index) => (
+            <Message
+              key={index}
+              index={index}
+              mes={mes}
+              deleteItem={props.deleteItem}
+            />
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
