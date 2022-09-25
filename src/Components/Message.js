5639b0852533b99489217e78a4c8feeecb2375ef
Maria T. Andersen <mteilmann93@gmail.com>
2022-09-25 21:26:20 +0200

    Add free fontAwesome icons to the project and add an icon to the delete message button.

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,8 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
 export default function List(props) {
   return (
     <li>
       {props.mes}
-      <button onClick={() => props.deleteItem(props.index)}>DELETE</button>
+      <button onClick={() => props.deleteItem(props.index)}>
+        DELETE <FontAwesomeIcon icon={faTrash} />
+      </button>
     </li>
   );
 }
