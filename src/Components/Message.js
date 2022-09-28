1e4bcf14d625ffea7371819890a89a569690e098
Maria T. Andersen <mteilmann93@gmail.com>
2022-09-28 21:41:04 +0200

    Alter between displaying input or span element depending on the boolean value of a state variable.

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -4,11 +4,25 @@ import {
   faTrash,
   faFloppyDisk,
 } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-export default function List(props) {
+export default function Message(props) {
+  const [mes, setMes] = useState(props.mes.text);
+
+  function EditMode(props) {
+    const isBeingEdited = props.isBeingEdited;
+    if (isBeingEdited) {
+      return (
+        <input value={props.text} onChange={(e) => setMes(e.target.value)} />
+      );
+    }
+    return <span>{props.text}</span>;
+  }
+
   return (
     <li>
-      {props.mes.text}
+      {/* {props.mes.text} */}
+      <EditMode isBeingEdited={props.mes.editMode} text={mes} />
       {!props.mes.editMode ? (
         <button
           onClick={() => {
@@ -21,6 +35,7 @@ export default function List(props) {
         <button
           onClick={() => {
             props.alterEditMode(props.mes);
+            // console.log(props.mes.editMode);
           }}
         >
           SAVE <FontAwesomeIcon icon={faFloppyDisk} />
