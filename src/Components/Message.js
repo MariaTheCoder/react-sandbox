67b4862905086857864ffbd3a18493ad1ff54e03
Maria T. Andersen <mteilmann93@gmail.com>
2022-09-29 18:13:42 +0200

    Implement conditional edit mode.

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -10,35 +10,42 @@ export default function Message(props) {
   const [mes, setMes] = useState(props.mes.text);
 
   function EditMode(props) {
-    const isBeingEdited = props.isBeingEdited;
+    const isBeingEdited = props.messageObject.editMode;
     if (isBeingEdited) {
       return (
-        <input value={props.text} onChange={(e) => setMes(e.target.value)} />
+        <>
+          <input value={props.text} onChange={(e) => setMes(e.target.value)} />
+          <button
+            onClick={() => {
+              props.alterEditMode(props.messageObject);
+            }}
+          >
+            SAVE <FontAwesomeIcon icon={faFloppyDisk} />
+          </button>
+        </>
       );
     }
-    return <span>{props.text}</span>;
-  }
-
-  return (
-    <li>
-      <EditMode isBeingEdited={props.mes.editMode} text={mes} />
-      {!props.mes.editMode ? (
+    return (
+      <>
+        <span>{props.text}</span>
         <button
           onClick={() => {
-            props.alterEditMode(props.mes);
+            props.alterEditMode(props.messageObject);
           }}
         >
           EDIT <FontAwesomeIcon icon={faEraser} />
         </button>
-      ) : (
-        <button
-          onClick={() => {
-            props.alterEditMode(props.mes);
-          }}
-        >
-          SAVE <FontAwesomeIcon icon={faFloppyDisk} />
-        </button>
-      )}
+      </>
+    );
+  }
+
+  return (
+    <li>
+      <EditMode
+        messageObject={props.mes}
+        text={mes}
+        alterEditMode={props.alterEditMode}
+      />
       <button onClick={() => props.deleteItem(props.index)}>
         DELETE <FontAwesomeIcon icon={faTrash} />
       </button>
